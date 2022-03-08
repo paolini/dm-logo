2f4ad45671c2f868c1e60c867513ce0f4202528e
Emanuele Paolini <emanuele.paolini@gmail.com>
2022-03-08 15:58:48 +0100

    altre varianti

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-function draw(ctx, N=5, dots=true) {
+function draw(ctx, N=5, dots=true, squares=true) {
     const margin = 0.2;
-    const r0 = 0.2;
+    const r0 = 0.25;
+    const blue = "#0000FF";
+    const white = "#FFFFFF";
+    const gray = white; //"#A0A0A0";
     let ii = 0;
     let i = -margin;
     let jj = 1;
     let j = 1;
     ctx.save();
     ctx.translate(200,370);
+    ctx.lineWidth=0.1;
     ctx.scale(30,30);
     for(let k=0;k<=N;++k) {        
-        ctx.fillStyle = "#0000FF";
+        if (squares) {
+            ctx.beginPath();
+            ctx.moveTo(0,0);
+            ctx.lineTo(j,0);
+            ctx.lineTo(j,j);
+            ctx.lineTo(0,j);
+            ctx.closePath();
+            ctx.strokeStyle=blue;
+            ctx.fillStyle = gray;
+            ctx.fill();
+            ctx.stroke();
+        }
+
+        ctx.fillStyle = blue;
         ctx.beginPath();
         ctx.moveTo(0,0);
         ctx.lineTo(j,0);
@@ -19,7 +36,8 @@ function draw(ctx, N=5, dots=true) {
 
         if (dots) {
             // disegna jj punti.
-            ctx.fillStyle = "#FFFFFF";
+            if (squares) ctx.fillStyle = gray;
+            else ctx.fillStyle = white;
             const r = r0;
             const R = j-r-r0;
             for(let x=0;x<jj;++x) {
@@ -42,6 +60,10 @@ function draw(ctx, N=5, dots=true) {
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-draw(ctx,5,false);
+draw(ctx,5,false,false);
 ctx.translate(400,0);
-draw(ctx,5,true);
+draw(ctx,5,true,false);
+ctx.translate(400,0);
+draw(ctx,5,false,true);
+ctx.translate(400,0);
+draw(ctx,5,true,true);
